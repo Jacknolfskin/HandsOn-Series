02f0bbc25b114a129757b28326b6aace996aaddd
feihu5 <1352490133@qq.com>
2018-09-30 10:45:39 +0800

    时间服务器优化完善

--- src/main/java/com/personal/handson/aio/time/TimeClient.java
+++ src/main/java/com/personal/handson/aio/time/TimeClient.java
@@ -6,13 +6,16 @@ package com.personal.handson.aio.time;
  * @Description:
  */
 public class TimeClient {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         int port = 8080;
         if (args != null && args.length > 0) {
             port = Integer.valueOf(args[0]);
         }
+        while (true){
+            new Thread(new AsyncTimeClientHandler("127.0.0.1",port),"AIO-AayncTimeClientHandler-001").start();
+            Thread.sleep(1000);
+        }
 
-        new Thread(new AsyncTimeClientHandler("127.0.0.1",port),"AIO-AayncTimeClientHandler-001").start();
     }
 
 }
